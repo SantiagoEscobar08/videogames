3c7e924799620046e2831121865bb93e613d72b9
Santiago <santiagoescobar451@gmail.com>
2024-08-23 21:01:27 -0500

    Script phyton 1

---
+++ Script_Python/Example1.py
@@ -0,0 +1,18 @@
+#Este es un comentario en linea
+
+'''
+Esto es un comentario de bloque
+'''
+
+data = '''
+Esto es una variable String en bloque
+'''
+
+n1 = 10
+n2 = 20
+suma = n1+n2
+
+print(suma)
+print("La suma es: ", suma)
+print(f"la suma es: {suma}")
+print("La suma es: ", n1 + n2)
