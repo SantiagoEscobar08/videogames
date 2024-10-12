9060eaa19b6c8c8c790cf9abeb09ba943abfd67e
Santiago <santiagoescobar451@gmail.com>
2024-10-11 20:20:30 -0500

    main

--- Script_Python/number_race/main.py
+++ Script_Python/number_race/main.py
@@ -1,8 +1,36 @@
 
 from random import randint
+import os
 
+status_game=True
+
+def main_menu():
+    print("::: MAIN MENU :::")
+    print("[1]. Start Game")
+    print("[2]. About us")
+    print("[3]. Exit")
+    opt = int(input("Press any option: "))
+    return opt
+
+while status_game:
+    os.system('clear')
+    op = main_menu()
+    if op == 1:
+        print("Game under construction")
+        key = input("Press any key to go to the main menu...")
+    elif op == 2:
+        print("Help under construction")
+        key = input("Press any key to go to the main menu...")
+    else:
+        print("See you later")
+        key = input("Press any key to exit...")
+        break
+        
+
+'''
 dice1= randint(1,6)
 dice2= randint(1,6)
 
 print(f"Dice 1: {dice1}")
 print(f"Dice 2: {dice2}")
+'''
